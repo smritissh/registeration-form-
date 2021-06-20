effae1a976bbca472bc71af9636abb1686d0ae6f
smritissh <smriti.ssh@gmail.com>
2021-06-20 19:03:07 +0530

    2 files modified, 1 added

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -8,6 +9,7 @@ import { Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   registerationForm: FormGroup;
+
   constructor(private fb: FormBuilder) {}
 
   ngOnInit() {}
