bfca5ee243313236893277963079b002e1a2fb74
smritissh <smriti.ssh@gmail.com>
2021-06-20 20:00:44 +0530

    2 files modified

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,9 +8,38 @@ import { Validators } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  registerationForm: FormGroup;
+    productForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) {
+    this.productForm = this.fb.group({
+      name: '',
+      quantities: this.fb.array([]) ,
+    });
+  }
+  
+  quantities() : FormArray {
+    return this.productForm.get("quantities") as FormArray
+  }
+   
+  newQuantity(): FormGroup {
+    return this.fb.group({
+      qty: '',
+      price: '',
+    })
+  }
+   
+  addQuantity() {
+    this.quantities().push(this.newQuantity());
+  }
+   
+  removeQuantity(i:number) {
+    this.quantities().removeAt(i);
+  }
+   
+  onSubmit() {
+    console.log(this.productForm.value);
+  }
+  }
 
   ngOnInit() {}
 }
